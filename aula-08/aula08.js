414d84dd823f4462e6f241e2c0ca2c471b2e7414
matheusporezeli <matheusporezeli@gmail.com>
2025-11-14 21:43:02 -0300

    14-11

--- aula-08/aula08.js
+++ aula-08/aula08.js
@@ -1,9 +1,12 @@
-const lista = document.getElementsByClassName("list");
+const lista = document.getElementById("list");
 
 function adicionarItem() {
-    
+    const novaLi = document.createElement("li");
+    novaLi.textContent = "Novo item";
+    lista.appendChild(novaLi);
 }
 
 function removerItem() {
-    
+    const ultimoItem = lista.lastElementChild;
+    lista.removeChild(ultimoItem);
 }
